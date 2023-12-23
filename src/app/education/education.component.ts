fa2478f9050be1f6e8990e9d449fa7be3fbd8827
harshu1210 <charshal40@gmail.com>
2023-12-24 00:06:13 +0530

    Education-Theme-and-Details-Template-Changes-Minor

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,25 +1,33 @@
 import { Component } from '@angular/core';
-interface EventItem {
-  status?: string;
-  date?: string;
-  icon?: string;
-  color?: string;
-  image?: string;
-}
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
-  events: EventItem[];
+  events: any[];
 
-    constructor() {
-        this.events = [
-            { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
-            { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
-            { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
-            { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
-        ];
-    }
+  constructor() {
+    this.events = [
+      {
+        schoolName: "PCET's Nutan Maharashtra Institute of Enginnering and Technology",
+        schoolLogo: "../../assets/images/nutan.jpeg",
+        startDate: "Jul 2018",
+        endDate: "Jun 2022",
+        icon: 'pi pi-check-circle',
+        color: 'white',
+        stream: "Computer & Science Engineering",
+        university: "Savitribai Phule Pune University",
+        cgpa: 9.34,
+        collegeMapLink: "https://maps.app.goo.gl/WhKJF3t9fmiFsN2j9",
+        universityMapLink: "https://maps.app.goo.gl/WfuC44xsxVMJxADy9",
+        collegeSite: "https://www.nmiet.edu.in/",
+        universitySite: "http://www.unipune.ac.in/",
+        activitesAndSocities: ["Football", "Volleyball", "Team Technothon"],
+        description: "Computer engineering combines electrical engineering, computer science, and mathematics to design and develop computer systems, hardware, and software. Computer engineers design hardware components, such as processors, memory systems, and input/output devices, and develop software applications for a variety of platforms and systems. They conduct research and testing to evaluate the performance and efficiency of computer systems, work collaboratively with others to develop new products and technologies and troubleshoot and debug computer systems. Computer engineers must also ensure that computer systems are secure and meet regulatory standards.",
+        skill: ["Machine Learning", "Databases", "Cloud Computing", "Data Structures", "Python (Programming Language)", "TensorFlow", "MySQL", "HTML5", "Cascading Style Sheets (CSS)", "JavaScript", "Web Development", "C (Programming Language)", "C++", "Engineering", "Java", "Database Management System (DBMS)", "PHP"
+        ]
+      },
+    ];
+  }
 }
