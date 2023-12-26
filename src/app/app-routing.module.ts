ca15a05cdabc42f23a3177851dec0e15f91eca00
harshu1210 <charshal40@gmail.com>
2023-12-26 18:50:19 +0530

    Tech-Stack-Component-Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EducationComponent } from './education/education.component';
+import { TechstackComponent } from './techstack/techstack.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path:'home', component: HomeComponent },
-  { path:'education', component:EducationComponent}
+  { path:'education', component:EducationComponent},
+  { path:'techStack', component:TechstackComponent}
 ];
 
 @NgModule({
